da0510fd8ed7d7ff03f8a77201183bbcbbd9e34a
Alan Massaro <alan.massar@gmail.com>
2021-12-29 00:12:59 +0100

    removed h1 in login

--- components/Login.js
+++ components/Login.js
@@ -4,7 +4,6 @@ const Login = () => {
     const { authenticate } = useMoralis();
     return (
         <div className="bg-black relative text-white h-screen">
-            <h1>I am the login screen</h1>
             <div className="flex flex-col justify-center items-center h-4/6">
                 <button onClick={authenticate} className="bg-yellow-500 p-5 rounded-lg font-bold hover:animate-pulse">Login with Metamask</button>
             </div>
