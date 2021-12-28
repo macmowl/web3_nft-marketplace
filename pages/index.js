5fa0c495d701942756e1331919b3d1ad686e6f03
Alan Massaro <alan.massar@gmail.com>
2021-12-29 00:12:01 +0100

    app

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,26 @@ import Login from '../components/Login';
 import { useMoralis } from 'react-moralis';
 
 export default function Home() {
-  const { isAuthenticated, logout } = useMoralis();
+  const { isAuthenticated, logout, user } = useMoralis();
 
   if (!isAuthenticated) return <Login />
 
   return (
-    <div className="h-screen">
+    <div className="h-screen flex justify-center items-center">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to the app</h1>
-      <button onClick={logout}>Logout</button>
+      <div className='flex flex-col items-center space-y-5'>
+        <h1>BOOOOOOOOOM connecté</h1>
+        <div className='text-center'>
+          <p>Bienvenue <span className="text-green-700">{user.get("username")}</span></p>
+          <p>Ouais, ton username est naze</p>
+        </div>
+        
+        <button onClick={logout} className='px-5 py-3 bg-red-400 rounded-lg font-bold text-white'>Logout</button>
+      </div>
+      
     </div>
   )
 }
