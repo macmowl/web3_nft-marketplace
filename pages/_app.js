446de11e0cea76f7e929421c05acee9c4c5906d0
Alan Massaro <alan.massar@gmail.com>
2021-12-30 18:00:32 +0100

    Smart contract + ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
-import { MoralisProvider } from "react-moralis";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-      <MoralisProvider appId={process.env.NEXT_PUBLIC_APP_ID} serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
         <Component {...pageProps} />
-      </MoralisProvider>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,93 @@
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import Massabots from '../src/artifacts/contracts/Massabots.sol/Massabots.json';
 import Head from 'next/head';
-import Login from '../components/Login';
-import { useMoralis } from 'react-moralis';
+
+const MSBAdress = process.env.NEXT_PUBLIC_MSB_ADRESS;
 
 export default function Home() {
-  const { isAuthenticated, logout, user } = useMoralis();
+  const [error, setError] = useState('');
+  const [data, setData] = useState({});
+  const [account, setAccount] = useState([]);
 
-  if (!isAuthenticated) return <Login />
+  useEffect(() => {
+    fetchData();
+    getAccounts();
+  }, [])
+
+  const getAccounts = async () => {
+    if(typeof window.ethereum !== 'undefined') {
+      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setAccount(accounts);
+      console.log(accounts[0]);
+    }
+  }
+
+  const fetchData = async () => {
+    if(typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(MSBAdress, Massabots.abi, provider);
+      try {
+        const cost = await contract.cost();
+        const totalSupply = await contract.totalSupply();
+        const object = {"cost": String(cost), "totalSupply": String(totalSupply)};
+        setData(object);
+      } catch(err) {
+        setError(err);
+      }
+    }
+  }
+
+  const mint = async () => {
+    if(typeof window.ethereum !== 'undefined') {
+      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(MSBAdress, Massabots.abi, signer);
+      try {
+        let overrides = {
+          from: accounts[0],
+          value: data.cost
+        }
+        const transaction = await contract.mint(accounts[0], 1, overrides);
+        await transaction.wait();
+        fetchData();
+      } catch(err) {
+        setError(err);
+      }
+    }
+  }
+
+  const withdraw = async () => {
+    if(typeof window.ethereum !== 'undefined') {
+      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(MSBAdress, Massabots.abi, signer);
+      try {
+        const transaction = await contract.withdraw();
+        await transaction.wait();
+      } catch(err) {
+        setError(err);
+      }
+    }
+  }
 
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex flex-col justify-center items-center">
       <Head>
-        <title>Create Next App</title>
+        <title>Create a stunning web3 app with cats</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='flex flex-col items-center space-y-5'>
-        <h1>BOOOOOOOOOM connecté</h1>
-        <div className='text-center'>
-          <p>Bienvenue <span className="text-green-700">{user.get("username")}</span></p>
-          <p>Ouais, ton username est naze</p>
-        </div>
-        
-        <button onClick={logout} className='px-5 py-3 bg-red-400 rounded-lg font-bold text-white'>Logout</button>
-      </div>
+      {error && <p>{error}</p>}
+      <h1 className='font-bold text-4xl mb-6'>Mint a Massabot NFT</h1>
+      <p><span className='font-bold text-blue-600 text-2xl'>{data.totalSupply}</span> /30</p>
+      <p>Each Massabot NFT costs {data.cost / 10**18} eth</p>
+      <p className='text-sm text-gray-600 font-light'>(excluding gas fees)</p>
+      <button onClick={mint} className="bg-blue-600 px-5 py-3 text-white font-bold rounded-lg mt-6">Buy one Massabot NFT</button>
+      {account[0] == process.env.NEXT_PUBLIC_OWNER_ADRESS && 
+        <button onClick={withdraw} className="bg-orange-600 px-4 py-2 text-white text-xs font-bold rounded-lg mt-6">Withdraw</button>
+      }
       
     </div>
   )
